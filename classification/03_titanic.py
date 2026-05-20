8b9291401b8fd179eddaf665ae4c971cad60f142
joshuaboladuro <joshb971@outlook.com>
2026-05-20 22:25:21 +0100

    Split training data into x and y and cleaned up variable names

--- classification/03_titanic.py
+++ classification/03_titanic.py
@@ -30,28 +30,38 @@ np.random.seed(42)
 # --- Solution ---
 # Start writing your solution here.
 # 1) load the data
-train_data = pd.read_csv("classification/train.csv")
-test_data = pd.read_csv("classification/test.csv")
+titanic_data = pd.read_csv("classification/train.csv")
 
 #2) inspect, clean and encode the data
-#print(train_data.head().to_string())
-#print(train_data.info()) # Expected: 889 samples. Age: 714, Cabin: 204, Embarked: 889
-#print(train_data.describe())
-#print(train_data.shape)
+#print(titanic_data.head().to_string())
+#print(titanic_data.info()) # Expected: 889 samples. Age: 714, Cabin: 204, Embarked: 889
+#print(titanic_data.describe())
+#print(titanic_data.shape)
 
 #Next steps:
 #Remove Cabin column (204), Remove additional Embarked rows (2), Reomve Age values (177) rows
-train_data_clean = train_data.drop(columns = ["Cabin"])
-#print(train_data_clean[train_data_clean['Embarked'].isna()].to_string())
-train_data_clean = train_data_clean.dropna(subset=["Embarked"])
-median_age = train_data_clean['Age'].median()
-train_data_clean["Age"] = train_data_clean["Age"].fillna(median_age)
+titanic_data_clean = titanic_data.drop(columns = ["Cabin"])
+#print(titanic_data_clean[titanic_data_clean['Embarked'].isna()].to_string())
+titanic_data_clean = titanic_data_clean.dropna(subset=["Embarked"])
+median_age = titanic_data_clean['Age'].median()
+titanic_data_clean["Age"] = titanic_data_clean["Age"].fillna(median_age)
 
-train_data_clean["Sex"] = train_data_clean["Sex"].map({"male":0, "female": 1})
-train_data_clean = pd.get_dummies(train_data_clean, columns=["Embarked"])
-train_data_clean["Embarked_C"] = train_data_clean["Embarked_C"].map({False:0, True: 1})
-train_data_clean["Embarked_Q"] = train_data_clean["Embarked_Q"].map({False:0, True: 1})
-train_data_clean["Embarked_S"] = train_data_clean["Embarked_S"].map({False:0, True: 1})
+titanic_data_clean["Sex"] = titanic_data_clean["Sex"].map({"male":0, "female": 1})
+titanic_data_clean = pd.get_dummies(titanic_data_clean, columns=["Embarked"])
+titanic_data_clean["Embarked_C"] = titanic_data_clean["Embarked_C"].map({False:0, True: 1})
+titanic_data_clean["Embarked_Q"] = titanic_data_clean["Embarked_Q"].map({False:0, True: 1})
+titanic_data_clean["Embarked_S"] = titanic_data_clean["Embarked_S"].map({False:0, True: 1})
+titanic_data_clean = titanic_data_clean.drop(columns=["PassengerId", "Name", "Ticket"])
 
-train_data_clean = train_data_clean.drop(columns=["PassengerId", "Name", "Ticket"])
-print (train_data_clean.head().to_string())
+print (titanic_data_clean.head().to_string())
+
+#3) split into X and y and train test split
+
+train_y = titanic_data_clean["Survived"]
+train_x = titanic_data_clean.drop(columns=["Survived"])
+test_x = pd.read_csv("classification/test.csv")
+
+
+
+
+
